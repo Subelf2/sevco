b7f18fede4c33702c18b2c5db14b614ca430c250
Clement Riom <riom.clement@gmail.com>
2026-06-14 22:29:02 +0200

    feat: removed files

--- server/config_template.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "TOKEN"
-
-DATABASE_FILE = "votes.db"
-
-PRIVATE_KEY_FILE = "server_private.pem"
-PUBLIC_KEY_FILE = "server_public.pem"
--- server/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass
-class VoteSession:
-    session_id: str
-    candidates: list[str]
-    end_time: datetime
